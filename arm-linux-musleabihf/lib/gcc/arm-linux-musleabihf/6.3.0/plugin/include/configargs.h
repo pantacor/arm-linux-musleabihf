ca5c8c2311524e2606d6927df8b10b4e57d225b5
gaurav-git <gauravpathak129@gmail.com>
2020-08-25 09:50:17 +0530

    Adding arm-linux-musleabihf toolchain

---
+++ arm-linux-musleabihf/lib/gcc/arm-linux-musleabihf/6.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../src_toolchain/configure --enable-languages=c,c++ --with-float=hard --disable-werror --target=arm-linux-musleabihf --prefix= --libdir=/lib --disable-multilib --with-sysroot=/arm-linux-musleabihf --enable-tls --disable-libmudflap --disable-libsanitizer --disable-gnu-indirect-function --disable-libmpx --enable-deterministic-archives --enable-libstdcxx-time --with-build-sysroot=/build/build/local/arm-linux-musleabihf/obj_sysroot";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "float", "hard" }, { "tls", "gnu" } };
